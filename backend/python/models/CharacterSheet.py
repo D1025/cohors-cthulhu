9e115f7cf275ccb1a8f7bb5b4dba26ee2a1fb7bf
Patryk Treszczotko <patryk5531@gmail.com>
2023-09-13 21:37:33 +0200

    feat: dabatase

--- backend/python/models/CharacterSheet.py
+++ backend/python/models/CharacterSheet.py
@@ -1,7 +1,7 @@
-from sqlalchemy import UUID, Column, ForeignKey, Integer, String, Uuid
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ARRAY, UUID, Column, ForeignKey, Integer, String, Uuid
+from sqlalchemy.orm import relationship
 
-Base = declarative_base()
+from backend.python.models.BaseDatabase import Base
 
 class characterSheet(Base):
     __tablename__ = 'character_sheet'
@@ -9,4 +9,18 @@ class characterSheet(Base):
     name = Column(String)
     description = Column(String)
     attributes = Column(UUID, ForeignKey('attributes.id'))
-    
+    languages = Column(ARRAY(String))
+    actualStress = Column(Integer)
+    maxStress = Column(Integer)
+    actualInjuries = Column(Integer)
+    maxInjuries = Column(Integer)
+    armour = Column(Integer)
+    courage = Column(Integer)
+    caste = Column(String)
+    resources = Column(Integer)
+    background = Column(String)
+    personalAgenda = Column(String)
+    characteristic = Column(String)
+    talents = Column(UUID, ForeignKey('talents.id'))
+    weapons = Column(UUID, ForeignKey('weapons.id'))
+    equipment = relationship('equipment', backref='character_sheet')
