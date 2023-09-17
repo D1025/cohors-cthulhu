f7d8c4aaf920858f6fcf9dfd7ea98afff430dd0e
Donald <patryk5531@gmail.com>
2023-09-17 15:15:54 +0200

    feat: added cosuss and herodion

---
+++ backend/python/database/Akusaa.py
@@ -0,0 +1,194 @@
+from enums.WeaponType import WeaponType
+from models.CharacterSheet import CharacterSheet
+from models.Attributes import Attributes
+from models.Talents import Talents
+from models.Weapons import Weapons
+from models.Equipment import Equipment
+from models.Skills import Focus, Skills
+from models.Truths import Truths
+
+def add_akussa_character_sheet(session):
+    try:
+
+
+        weapons1 = Weapons(
+            name = "Cudgel",
+            weaponType = WeaponType.MELEE,
+            range = "Reach 2",
+            damage = 3,
+            effects = "Stun"  
+        )
+        
+        weapons2 = Weapons(
+            name = "Sling",
+            weaponType = WeaponType.THROWN,
+            range = "Medium range",
+            damage = 5,
+            effects = "Stun, Innacurate, Subtle"
+        )
+        
+        session.add_all([weapons1, weapons2])
+
+        equipment1 = Equipment(
+            name = "Plain Traveling Clothes",
+
+        )
+        
+        equipment2 = Equipment(
+            name = "Suit of Fine Clothing (stowed)"
+        )
+        
+        equipment3 = Equipment(
+            name = "Personal Library",
+            description = "Choice tomes and reference materials."
+        )
+        
+        equipment4 = Equipment(
+            name = "Pet Mouse",
+            description = "A small gray mouse named Ruhig, often hidden in Akusaa’s clothing."
+        )
+        
+        equipment5 = Equipment(
+            name = "Writing Materials and Tools",
+            description = "Includes vellum or papyrus scrolls and books"
+        )
+        
+        equipment6 = Equipment(
+            name = "Stylus and Wax Tablet",
+            description = "To make quick, impermanent notes, writers often employ a bronze stylus with a point on one end and a flat spade on the other, scribing into sheets of wax contained in a wooden or metal box. When a record is no longer needed, they use the stylus’s spade to wipe out the markings."
+        )
+            
+        
+        session.add_all([equipment1, equipment2, equipment3, equipment4, equipment5, equipment6])
+
+        attributes = Attributes(
+            agility=6,
+            brawn=6,
+            coordination=9,
+            gravitas=9,
+            insight=11,
+            reason=11,
+            will=10
+        )
+        session.add(attributes)
+
+        skills = Skills(
+            academia = 4,
+            athletics = 0,
+            crafting = 2,
+            engineering = 2,
+            fighting = 0,
+            medicine = 2,
+            observation = 3,
+            persuasion = 2,
+            resilience = 2,
+            stealth = 0,
+            survival = 0,
+            tactics = 1
+        )
+        focus1 = Focus(
+            skill_name = "academia",
+            focus_name = "History"
+        )
+        
+        focus2 = Focus(
+            skill_name = "academia",
+            focus_name = "Linguistics"
+        )
+        
+        focus3 = Focus(
+            skill_name = "academia",
+            focus_name = "Religion"
+        )
+        focus4 = Focus(
+            skill_name = "observation",
+            focus_name = "Sight"
+        )
+
+        skills.focus.extend([focus1, focus2, focus3, focus4])
+        session.add_all([skills, focus1, focus2, focus3, focus4])
+        
+        talents1 = Talents(
+            name = "Applied Knowledge",
+            description = "The knowledge you have accumulated gives you bursts of insight to deal with unexpected challenges. Once per scene, you may use Academia in place of another skill when you make a skill test, and you count as having an appropriate focus for that test."
+        )
+        
+        talents2 = Talents(
+            name = "Fade Away",
+            description = "Once per scene as a minor action, you can generate 2 Threat for the GM’s Threat pool to slip from sight. If you do so, you cannot be the target of an attack (other than attacks with the Area damage effect) until the start of your next turn. If you are behind heavy cover (such as a large tree trunk or short stone wall) when you use this talent, you do not need to generate Threat."
+        )
+        
+        talents3 = Talents(
+            name = "Foreboding Survival",
+            description = "Every so often, it feels like you narrowly avoid disaster thanks to an unknown guiding hand. How you avoid these fates is unknown, but bad luck seems to befall those around you. Once per session when you are dealt an injury, you may generate 3 Threat for the GM’s Threat pool in order to avoid suffering that injury. At the GM’s discretion, you may receive opportunities to avoid other misfortune as well in exchange for generating 3 Threat."
+        )
+        
+        talents4 = Talents(
+            name = "Vero Nihil Verius",
+            description = "You have a keen eye that can spot the truth of a matter. Whenever you make a skill test to detect danger or hidden enemies, reduce the difficulty by 1."
+        )
+        
+        talents5 = Talents(
+            name = "Writ in Stone",
+            description = "Your homeland is known for its long history, some of which is etched into stone on the faces of ancient walls. When you make an Academia-based test to recall a bit of relevant history or lore, you can re-roll any dice that fail to produce a success. "
+        )
+        
+        session.add_all([talents1, talents2, talents3, talents4, talents5])
+        
+        truths1 = Truths(
+            description = "Aegyptus"
+        )
+        
+        truths2 = Truths(
+            description = "Dreamwalker"
+        )
+        
+        truths3 = Truths(
+            description = "Seeking the Lost"
+        )
+        
+        truths4 = Truths(
+            description = "Student of the Occult"
+        )
+        
+        truths5 = Truths(
+            description = "I Have a Secret"
+        )
+        
+        session.add_all([truths1, truths2, truths3, truths4, truths5])
+        
+        # Dodawanie arkusza postaci
+        character_sheet = CharacterSheet(
+            name="Akusaa",
+            nickname = "Aldis",
+            description="""Aegyptus Scholar and Occultist (Sage)
+A highly knowledgeable linguist and occultist originally from Aegyptus, Akusaa is a woman in her early thirties who left that area under some duress. She was accused of stealing sacred tomes, a crime she did not actually commit, and is now an outcast from her homeland. She supports herself by taking work as a translator but is more interested in seeking out lore regarding Germanic mysticism in the frontier region, which is what has brought her to the Laurium area. She is on the hunt for mystically potent relics to investigate a theory she has on the ways the gods empower and guide favored mortals.
+Akusaa is polite and well-spoken, though prone to conversing a touch too formally and made uncomfortable by some of the rugged and unfamiliar habits of those living in the frontier region. She prefers to adopt an androgynous appearance and feels uneasy when people focus on her femininity. 
+She has had some small brushes with the supernatural, particularly in her dreams, which have taken her to strange and vividly inexplicable places. She is convinced the gods are active participants in mortal affairs, not always to mortals’ benefit. She finds conflict distasteful and seeks others to help her defend herself against threats that may arise in the course of her pursuits.""",
+            actualStress=0,
+            maxStress=11,
+            actualInjuries=0,
+            maxInjuries=3,
+            armour=0,
+            courage=2,
+            languages=" Coptic, Aramaic, Celtic, Germanic, Greek, Latin, Punic",
+            caste="Outcast",
+            resources=0,
+            background="Scholar",
+            personalAgenda="Acquire an Object. An important item or relic has gone missing in the wilderness, and you aim to locate it.",
+            characteristic="Dreamwalker. Sleep is a mere gateway, and even as a child you wandered the Dreamlands.",
+            attributes=attributes,
+            equipment=[equipment1, equipment2, equipment3, equipment4, equipment5, equipment6],
+            weapons=[weapons1, weapons2],
+            talents=[talents1, talents2, talents3, talents4, talents5],
+            truths=[truths1, truths2, truths3, truths4, truths5],
+            skills=skills
+        )
+        session.add(character_sheet)
+
+
+        session.commit()
+        print("Record Akussa added successfully")
+    except Exception as e:
+        print(f"Błąd dodawania rekordu Akussa: {str(e)}")
+        session.rollback()
---
+++ backend/python/database/Cossus.py
@@ -0,0 +1,172 @@
+from enums.WeaponType import WeaponType
+from models.CharacterSheet import CharacterSheet
+from models.Attributes import Attributes
+from models.Talents import Talents
+from models.Weapons import Weapons
+from models.Equipment import Equipment
+from models.Skills import Focus, Skills
+from models.Truths import Truths
+
+def add_cossus_character_sheet(session):
+    try:
+        
+        weapons1 = Weapons(
+            name = "Gladius",
+            weaponType = WeaponType.MELEE,
+            range = "Reach 2",
+            damage = 6,
+            effects = "Piercing 1, Parrying"  
+        )
+        
+        weapons2 = Weapons(
+            name = "Pilum (3)",
+            weaponType = WeaponType.THROWN,
+            range = "Close range",
+            damage = 6,
+            effects = "Piercing 1, Special: On an effect [ ] result, after damage is dealt, the pilum is rendered unusable and if the target is carrying a shield, the pilum has embedded in the shield and lowers the shield’s Cover resistance by 1"
+        )
+        
+        weapons3 = Weapons(
+            name = "Large Shield",
+            weaponType = WeaponType.MELEE,
+            range = "Reach 1",
+            damage = 5,
+            effects = "Shield 3"  
+        )
+        
+        session.add_all([weapons1, weapons2, weapons3])
+
+        equipment1 = Equipment(
+            name = "Hooded Cloak",
+            description = "In addition to providing warmth and protection from the elements, this cloak has a deep hood that can help conceal a person’s identity. This cloak was looted on campaign and has become a favored and treasured item. It also serves as a good-luck charm. Once per adventure, you can invoke the good-luck charm the same way you would use a Fortune point."
+
+        )
+        
+        equipment2 = Equipment(
+            name = "Soldier’s Kit (skill kit)",
+            description = "Contains spare clothing, food rations, a small spade and cooking vessel, and supplies for making camp. This kit contains enough supplies to provide 3 bonus Momentum for buying d20s."
+        )
+        
+        session.add_all([equipment1, equipment2])
+
+        attributes = Attributes(
+            agility=9,
+            brawn=11,
+            coordination=9,
+            gravitas=7,
+            insight=10,
+            reason=7,
+            will=8
+        )
+        session.add(attributes)
+
+        skills = Skills(
+            academia = 0,
+            athletics = 3,
+            crafting = 0,
+            engineering = 0,
+            fighting = 3,
+            medicine = 0,
+            observation = 1,
+            persuasion = 0,
+            resilience = 4,
+            stealth = 0,
+            survival = 4,
+            tactics = 2
+        )
+        focus1 = Focus(
+            skill_name = "fighting",
+            focus_name = "Melee Weapons"
+        )
+        
+        focus2 = Focus(
+            skill_name = "fighting",
+            focus_name = "Thrown Weapons"
+        )
+        
+        focus3 = Focus(
+            skill_name = "resilience",
+            focus_name = "Discipline"
+        )
+        focus4 = Focus(
+            skill_name = "resilience",
+            focus_name = "Fortitude"
+        )
+
+        skills.focus.extend([focus1, focus2, focus3, focus4])
+        session.add_all([skills, focus1, focus2, focus3, focus4])
+        
+        talents1 = Talents(
+            name = "Alea Lacta Est (“The die is cast”)",
+            description = "Once per scene when an NPC suffers a complication, you can use this talent to increase the Threat cost to cancel the complication to 4 Threat."
+        )
+        
+        talents2 = Talents(
+            name = "Iron Hide",
+            description = "You possess great fortitude and can bear horrible injuries without showing it. For the purpose of increasing your complication range, you ignore the first injury suffered. In addition, you are defeated after suffering 4 injuries instead of 3."
+        )
+        
+        talents3 = Talents(
+            name = "Legionary Training",
+            description = "You may re-roll one d20 on any Fighting-based test you make with a spear, gladius, or pilum, though the second result stands."
+        )
+        
+        talents4 = Talents(
+            name = "Skin of Your Teeth",
+            description = "You have a knack for (just barely) avoiding the worst injuries. Once per scene, you may spend a Fortune point when you are dealt an injury, in order to avoid suffering that injury."
+        )
+        
+        session.add_all([talents1, talents2, talents3, talents4])
+        
+        truths1 = Truths(
+            description = "Roman Citizen"
+        )
+        
+        truths2 = Truths(
+            description = "Campaign Veteran"
+        )
+        
+        truths3 = Truths(
+            description = "Sole Survivor"
+        )
+        
+        truths4 = Truths(
+            description = "I Have a Secret"
+        )
+        
+        session.add_all([truths1, truths2, truths3, truths4])
+        
+        # Dodawanie arkusza postaci
+        character_sheet = CharacterSheet(
+            name="Cossus Atrius Thamugadus",
+            nickname = "Jan",
+            description="""North African Legionary (Soldier, Legionary Specialization)
+Cossus, a soldier in his twenties, is a Roman citizen and legionary, though he is originally from northern Africa and descends from ancestors who fought on the side of Carthage in the Punic Wars. He knows of these legends and enjoys speaking of the military exploits of his ancestors, but he is nonetheless a proud Roman, seeing his service with the I Minerva Legion along the frontier as fostering the extension of a great civilization. He is technically attached to one of the barracks closer to Bonna, but his superiors gave him leave to attend to personal matters. He has been seeking information regarding his brother, a fellow soldier who was posted to the garrison in Laurium but has gone missing under suspicious circumstances.
+Cossus is young but has seen some real action and has participated in some difficult battles, including one in which his squad was nearly entirely wiped out by Germanic rebels. The circumstances were such that he cannot blame the rebels for wanting to defend their territory, and the officer in charge made several blunders that led to the conflict, but nonetheless he is wary around more militant locals. He has a friendly demeanor and feels it is his job as a soldier to defend those who cannot protect themselves. He has sought to avoid involvement in any punitive actions taken by his legion against ordinary civilians.""",
+            actualStress=0,
+            maxStress=15,
+            actualInjuries=0,
+            maxInjuries=4,
+            armour=5,
+            courage=0,
+            languages="Latin, Greek (simple conversations), Punic",
+            caste="Plebian",
+            resources=2,
+            background="Campaign Veteran",
+            personalAgenda="Protect the Innocent. You use your strength to protect those who need it. When you perform actions to protect those who cannot defend themselves, you can re-roll 1d20 on opposed tests for which you are the reactive character.",
+            characteristic="Sole Survivor. You didn’t get a good look at who— or what—slaughtered nearly every living thing in the settlement, but for reasons you can’t fathom, you alone were spared.",
+            attributes=attributes,
+            equipment=[equipment1, equipment2],
+            weapons=[weapons1, weapons2],
+            talents=[talents1, talents2, talents3, talents4],
+            truths=[truths1, truths2, truths3, truths4],
+            skills=skills
+        )
+        session.add(character_sheet)
+
+
+        session.commit()
+        print("Record Cossus added successfully")
+    except Exception as e:
+        print(f"Błąd dodawania rekordu Cossus: {str(e)}")
+        session.rollback()
